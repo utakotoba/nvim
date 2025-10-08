5ce32979740d912d5487a690a7901697dba5edd2
Akiyama Kohane <echo.aimless@gmail.com>
2025-10-08 20:39:28 +0800

    chore: move plugin specs getter to core

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -9,6 +9,40 @@ function M.init()
   })
 end
 
+local function gather_plugin_specs()
+  local result = {}
+
+  local specs_path = vim.fn.stdpath('config') .. '/lua/plugins'
+
+  if not vim.uv.fs_stat(specs_path) then
+    -- return empty table if no spec
+    return result
+  end
+
+  local spec_files = vim.fn.globpath(specs_path, '*.lua', false, true)
+  
+  for _, filepath in ipairs(spec_files) do
+    -- trim file path to get file name without ext
+    local filename = vim.fn.fnamemodify(filepath, ':t:r')
+
+    local module_name = 'plugins.' .. filename
+
+    -- assume that all specs in `vim.pack.Spec` type
+    local ok, spec = pcall(require, module_name)
+
+    if ok then
+      table.insert(result, spec)
+    else
+      vim.notify(
+        string.format('Failed to load plugin spec: `%s`', module_name),
+        vim.log.levels.ERROR
+      )
+    end
+  end
+
+  return result
+end
+
 function M:load()
   -- indicator for plugin loading state
   self.is_ok = false
@@ -23,7 +57,7 @@ function M:load()
     self.is_ok = true
 
     -- load all specs under `plugins`
-    local specs = require('plugins').gather_plugin_specs()
+    local specs = gather_plugin_specs()
     if #specs ~= 0 then
       vim.pack.add(specs, {
         load = require('lz.n').load, -- custom loader for lazy-loading
