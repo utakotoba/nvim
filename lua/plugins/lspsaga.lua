eb2125c1a65911949c13ff014b615d59f435190a
Akiyama Kohane <echo.aimless@gmail.com>
2025-10-10 11:12:56 +0800

    chore: use `BufEnter` for lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -2,7 +2,9 @@ return {
   src = 'https://github.com/utakotoba/lspsaga.nvim.git',
   data = {
     lazy = true,
-    event = 'LspAttach',
+    -- use `BufEnter` instead of `LspAttach` to make sure
+    -- the time lspsaga load with buffer gui
+    event = 'BufEnter',
     after = function()
       vim.schedule(function()  
         require('lspsaga').setup({
@@ -12,6 +14,7 @@ return {
           },
           symbol_in_winbar = {
             show_file = false,
+            color_mode = false,
           },
         })
       end)
